1634ea842b86653f8846ab47782cfdc2838c80ab
Pillzan <oscar.thernstrom@hotmail.com>
2015-05-08 07:34:29 +0200

    + Added the [hungry command. it´s really just for testing that the hunger has been turned off.

---
+++ Scripts/Commands/Hungry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Server.Mobiles;
+
+namespace Server.Commands
+{
+    public class Hungry
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("hungry", AccessLevel.Player, new CommandEventHandler(Classe_OnCommand));
+        }
+
+        [Usage("hungry")]
+        [Description("Returns the players hunger level.")]
+        private static void Classe_OnCommand(CommandEventArgs e)
+        {
+            string retString = "Hunger isnt working as intended :(";
+            Mobile m = e.Mobile;
+
+            var hungerLevel = m.Hunger;
+            if (hungerLevel == 20) // hunger should always be 20. But im not 100% certain yet
+            {
+                retString = "You are not hungry at all";
+            }
+            
+            e.Mobile.SendMessage(retString);
+
+        }
+    }
+}
