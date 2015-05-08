e20d7d32be8ebadc2e3fd0114a740dd88a5ded07
Pillzan <oscar.thernstrom@hotmail.com>
2015-05-08 19:06:56 +0200

    + Added a Dice Throw class (i´m three bers in and i dunno if i am really helping :/ )

---
+++ Scripts/Custom/RandomClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Custom
+{
+    class RandomClass
+    {
+
+        Random r = new Random();
+
+        public int D6Roll(int Rolls)
+        {
+        int sum=0;
+        for (int i = 0; i < 4; i++)
+        {
+            var roll = r.Next(1, 7);
+            sum += roll;
+        }
+        return sum;
+        }
+
+        public int D8Roll(int Rolls)
+        {
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var roll = r.Next(1, 9);
+                sum += roll;
+            }
+            return sum;
+        }
+
+        public int D10Roll(int Rolls)
+        {
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var roll = r.Next(1, 11);
+                sum += roll;
+            }
+            return sum;
+        }
+
+        public int D12Roll(int Rolls)
+        {
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var roll = r.Next(1, 13);
+                sum += roll;
+            }
+            return sum;
+        }
+
+        public int D20Roll(int Rolls)
+        {
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var roll = r.Next(1, 21);
+                sum += roll;
+            }
+            return sum;
+        }
+    }
+}
