a12e41a9bbe2b3bc9e60bab7c7ee00c5c7ef3342
Oscar Ternström <oscar.thernstrom@hotmail.com>
2016-03-06 01:43:43 +0100

    + Fixed begging somewhat + Increased range of detecting hidden somewhat + Decreased itemid cooldown for specced mages + Fixed bug that poison charged weapons couldnt poison enemies ^^ + Fixed another bug with poison on weapons, now decreases the amount of charges when the weapon have applied it to an enemy + Fixed poisoning skill, specced theifs have more charges + Fixed some in detectinghidden, hiding, meditation, peacemaking + Boosted stealth for specced Theifs + Added ability to check the poison level with tasteid aswell as armslore + Fixed tracking, boosted range ALOT, made hidden players untrackable + Can no longer snoop/steal other players stuff

--- Scripts/Gumps/Zulugumps/necrobookgump.cs
+++ Scripts/Gumps/Zulugumps/necrobookgump.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Server.Items;
 using Server.Mobiles;
 using Server.Network;
+using Server.Spells.Zulu.NecroSpells;
 
 /*
  *  Author Oscar Ternström
@@ -13,11 +14,13 @@ namespace Server.Gumps.Zulugumps
 
     public class necrobookgump : Gump
     {
+        Mobile test;
         public necrobookgump(Mobile owner, bool[] array)
             : base(100, 0)
         {
             try
             {
+                test = owner;
                 int xName1 = 80;
                 int xName2 = 240;
                 int yName1 = 65;
@@ -146,6 +149,7 @@ namespace Server.Gumps.Zulugumps
         public override void OnResponse(NetState state, RelayInfo info)
         {
             Mobile from = state.Mobile;
+            Mobile caster = test;
             switch (info.ButtonID)
             {
                 case 1:
@@ -182,13 +186,16 @@ namespace Server.Gumps.Zulugumps
                     Console.WriteLine("Casting Wraithform");
                     break;
                 case 12:
-                    Console.WriteLine("Casting Wyvern Strike");
+                   // Console.WriteLine("Casting Wyvern Strike");
+                    new WyvernStrikeSpell(caster, null).Cast();
                     break;
                 case 13:
                     Console.WriteLine("Casting Kill");
+                    new KillSpell(caster, null).Cast();
                     break;
                 case 14:
-                    Console.WriteLine("Casting Liche");
+                    new LicheSpell(caster, null).Cast();
+                    //Console.WriteLine("Casting Liche");
                     break;
                 case 15:
                     Console.WriteLine("Casting Plague");
